2386a5014f10aaa138a7bbb500da2b7640edd510
Chen Guo <chen.guo.0625@gmail.com>
2020-04-03 10:21:53 -0700

    DPI-based font size

--- appearance.el
+++ appearance.el
@@ -35,3 +35,33 @@
 (display-time)
 
 (require 'rainbow-delimiters)
+
+;; font size by DPI
+;; https://emacs.stackexchange.com/a/44930
+(defun my-dpi (&optional frame)
+  "Get the DPI of FRAME (or current if nil)."
+  (cl-flet ((pyth (lambda (w h)
+                    (sqrt (+ (* w w)
+                             (* h h)))))
+            (mm2in (lambda (mm)
+                     (/ mm 25.4))))
+    (let* ((atts (frame-monitor-attributes frame))
+           (pix-w (cl-fourth (assoc 'geometry atts)))
+           (pix-h (cl-fifth (assoc 'geometry atts)))
+           (pix-d (pyth pix-w pix-h))
+           (mm-w (cl-second (assoc 'mm-size atts)))
+           (mm-h (cl-third (assoc 'mm-size atts)))
+           (mm-d (pyth mm-w mm-h)))
+      (/ pix-d (mm2in mm-d)))))
+
+(defun my-preferred-font-size ()
+  (let ((dpi (my-dpi)))
+    (message "dpi" dpi)
+    (cond
+     ((< dpi 110) 96)
+     ((< dpi 160) 128)
+     (t 175))))
+
+(set-face-attribute 'default nil
+                    :height
+                    (my-preferred-font-size))
